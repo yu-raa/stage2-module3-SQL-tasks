39cb07e57bb0624ad5ca20273fa406143a63c27e
Yuliya Raikova <yuradev2001@gmail.com>
2023-01-02 19:24:36 +0300

    ?..

--- lesson1/src/main/resources/SOLUTION.sql
+++ lesson1/src/main/resources/SOLUTION.sql
@@ -16,14 +16,18 @@ name varchar(45)
 );
 create table Payment (
 id bigint primary key,
-type_id bigint foreign key references PaymentType(id),
+type_id bigint,
 amount decimal,
-student_id bigint foreign key references Student(id),
-payment_date datetime
+student_id bigint,
+payment_date datetime,
+foreign key(type_id) references PaymentType(id),
+foreign key(student_id) references Student(id)
 );
 create table Mark (
 id bigint primary key,
-student_id bigint foreign key references Student(id),
-subject_id bigint foreign key references Subject(id),
-mark int
+student_id bigint,
+subject_id bigint,
+mark int,
+foreign key(student_id) references Student(id),
+foreign key(subject_id) references Subject(id)
 );
