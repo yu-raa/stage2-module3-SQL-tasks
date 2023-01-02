7e11b827db15990904c5cf87989d693116fe2c7c
Yuliya Raikova <yuradev2001@gmail.com>
2023-01-02 19:51:51 +0300

    ?..

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,9 +1,9 @@
-alter table student add constraint constr not null(birthday);
-alter table mark add constraint cons check (mark >= 1 and mark <= 10) (mark);
-alter table mark add constraint con not null (student_id);
-alter table mark add constraint co not null (subject_id);
-alter table subject add constraint constra check (grade >= 1 and grade <= 5) not null (grade);
-alter table paymenttype add constraint constrai unique (name);
-alter table payment add constraint constrain not null (type_id);
-alter table payment add constraint constrain2 not null (amount);
-alter table payment add constraint constrain3 not null (payment_date);
+alter table student add constraint not null(birthday);
+alter table mark add constraint check (mark >= 1 and mark <= 10) (mark);
+alter table mark add constraint not null (student_id);
+alter table mark add constraint not null (subject_id);
+alter table subject add constraint check (grade >= 1 and grade <= 5) not null (grade);
+alter table paymenttype add constraint unique (name);
+alter table payment add constraint not null (type_id);
+alter table payment add constraint not null (amount);
+alter table payment add constraint not null (payment_date);
