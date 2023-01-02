d4a6099ed39f84eb5332b0c1883a41fd5133b8c7
Yuliya Raikova <yuradev2001@gmail.com>
2023-01-02 19:35:53 +0300

    ?..

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,9 +1,9 @@
-alter table student modify birthday DATE not null;
-alter table mark modify mark int check (mark >= 1 and mark <= 10);
-alter table mark modify student_id bigint not null;
-alter table mark modify subject_id bigint not null;
-alter table subject modify grade int check (grade >= 1 and grade <= 5) not null;
-alter table paymenttype modify name varchar(45) unique;
-alter table payment modify type_id BIGINT not null;
-alter table payment modify amount decimal not null;
-alter table payment modify payment_date DateTime not null;
+alter table student modify column birthday DATE not null;
+alter table mark modify column mark int check (mark >= 1 and mark <= 10);
+alter table mark modify column student_id bigint not null;
+alter table mark modify column subject_id bigint not null;
+alter table subject modify column grade int check (grade >= 1 and grade <= 5) not null;
+alter table paymenttype modify column name varchar(45) unique;
+alter table payment modify column type_id BIGINT not null;
+alter table payment modify column amount decimal not null;
+alter table payment modify column payment_date DateTime not null;
