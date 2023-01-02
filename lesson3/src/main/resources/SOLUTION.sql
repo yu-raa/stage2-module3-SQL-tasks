f75cdcaf9db0d1da6ad333077aa43b63372ce3a8
Yuliya Raikova <yuradev2001@gmail.com>
2023-01-02 19:41:50 +0300

    ?..

--- lesson3/src/main/resources/SOLUTION.sql
+++ lesson3/src/main/resources/SOLUTION.sql
@@ -1,9 +1,9 @@
-alter table student modify column birthday DATE not null;
-alter table mark modify column mark int check (mark >= 1 and mark <= 10);
-alter table mark modify column student_id bigint not null;
-alter table mark modify column subject_id bigint not null;
-alter table subject modify column grade int check (grade >= 1 and grade <= 5) not null;
-alter table paymenttype modify column name varchar(45) unique;
-alter table payment modify column type_id BIGINT not null;
-alter table payment modify column amount decimal not null;
-alter table payment modify column payment_date DateTime not null;
+alter table student add constraint constr not null(birthday);
+alter table mark add constraint cons check (mark >= 1 and mark <= 10) (mark);
+alter table mark add constraint con not null (student_id);
+alter table mark add constraint co not null (subject_id);
+alter table subject add constraint constra check (grade >= 1 and grade <= 5) not null (grade);
+alter table paymenttype add constraint constrai unique (name);
+alter table payment add constraint constrain not null (type_id);
+alter table payment add constraint constrain2 not null (amount);
+alter table payment add constraint constrain3 not null (payment_date);
