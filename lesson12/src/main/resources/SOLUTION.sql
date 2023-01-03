877ed07ec5f8a16816bc2319cba4f418b2133aca
Yuliya Raikova <yuradev2001@gmail.com>
2023-01-03 10:57:09 +0300

    ?..

--- lesson12/src/main/resources/SOLUTION.sql
+++ lesson12/src/main/resources/SOLUTION.sql
@@ -1,5 +1,3 @@
-alter table student add grade int;
-delete from student where grade >= 4;
 delete from student where id in (select student_id from mark having (select count(*) from mark group by student_id where mark < 4) >= 1);
 delete from paymenttype where name = 'DAILY';
 delete from mark where mark < 7;
